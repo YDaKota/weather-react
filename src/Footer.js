1d0b9155e090df37f96253764c5f83eb161d7c1d
YDaKota <95923839+YDaKota@users.noreply.github.com>
2022-08-15 18:12:58 +0300

    Add correct link to Git

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer () {
     return (
     <small class="footer">
-        <a href="https://github.com/YDaKota/weather-project" target="_blank" rel="noopener noreferrer">Open source code</a> by Yulia
+        <a href="https://github.com/YDaKota/weather-react" target="_blank" rel="noopener noreferrer">Open source code</a> by Yulia
     </small>
     );
 }
